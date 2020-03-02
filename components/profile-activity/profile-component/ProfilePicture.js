3864540e7f834f9abb814937d47e66ba52aecf3e
William Chung <williamc825@hotmail.com>
2020-03-01 22:42:29 -0800

    added top text and stars to profile. WIP

--- components/profile-activity/profile-component/ProfilePicture.js
+++ components/profile-activity/profile-component/ProfilePicture.js
@@ -10,7 +10,7 @@ export default class ProfilePicture extends React.Component {
         return ( 
             <View>
                 <Image
-                    style={{width: 200, height: 200}}
+                    style={{width: 200, height: 200, borderRadius: 200, borderColor: 'yellow', borderWidth: 4}}
                     source={{uri: 'https://facebook.github.io/react-native/img/tiny_logo.png'}}
                 />
             </View>
